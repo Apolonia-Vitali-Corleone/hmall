f3cac4552574a3ff97e1b5badc18a2050ee0d768
Apolonia-Vitali-Corleone <jamesgaoctf@gmail.com>
2025-02-05 22:53:55 +0800

    1

---
+++ hmall-nginx/nginx.bat
@@ -0,0 +1,1 @@
+start F:\BaiduNetdiskDownload\hmall-nginx\nginx.exe
---
+++ hmall/hm-api/src/main/java/com/hmall/api/client/fallback/TradeClientFallback.java
@@ -0,0 +1,26 @@
+package com.hmall.api.client.fallback;
+
+import com.hmall.api.client.ItemClient;
+import com.hmall.api.client.TradeClient;
+import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
+import com.hmall.common.exception.BizIllegalException;
+import com.hmall.common.utils.CollUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.openfeign.FallbackFactory;
+
+import java.util.Collection;
+import java.util.List;
+
+@Slf4j
+public class TradeClientFallback implements FallbackFactory<TradeClient> {
+    @Override
+    public TradeClient create(Throwable cause) {
+        return new TradeClient() {
+            @Override
+            public void markOrderPaySuccess(Long orderId) {
+
+            }
+        };
+    }
+}
